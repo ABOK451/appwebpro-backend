16babfe53cea25e1cbcc84348122f2ff83bbb69c
ABOK451 <alancito2013america@gmail.com>
2025-11-10 19:28:31 -0600

    hola

--- src/interfaces/routes/loginRoutes.js
+++ src/interfaces/routes/loginRoutes.js
@@ -4,7 +4,7 @@ const { verificarSesionActiva } = require('../middlewares/sesionActiva');
 const {  loginUsuario,verificarCodigo, logout } = require('../controllers/authController');
 
 
-router.post("/auth/login",  loginUsuario);
+router.post("/auth/login", verificarSesionActiva, loginUsuario);
 router.post('/auth/verificar-codigo', verificarSesionActiva,verificarCodigo);
 router.post('/auth/logout', logout);
 
