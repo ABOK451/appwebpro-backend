c5c534f98df5f0f5de281d6f13af4bf1aceabe29
ABOK451 <alancito2013america@gmail.com>
2025-11-26 18:02:37 -0600

    frfr

--- src/server.js
+++ src/server.js
@@ -43,11 +43,7 @@ app.use((req, res, next) => {
 
 app.use(express.urlencoded({ extended: true }));
 
-// Logs de headers
-app.use((req, res, next) => {
-  console.log("[GLOBAL HEADERS]", req.headers);
-  next();
-});
+
 
 // CORS
 app.use(cors({
